959d44542e79a3be36a4d9df10aa7c4fccb4b8f2
raj <nagaraja.kambathalli@kaplan.edu>
2024-02-26 21:12:15 +0530

    useForm example

---
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -0,0 +1,23 @@
+import categories from "./catergories";
+
+interface Props {
+  onSelectCategory: (category: string) => void;
+}
+
+const ExpenseFilter = ({ onSelectCategory }: Props) => {
+  return (
+    <select
+      className="form-select"
+      onChange={(e) => onSelectCategory(e.target.value)}
+    >
+      <option value="">All categories</option>
+      {categories.map((category) => (
+        <option key={category} value={category}>
+          {category}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default ExpenseFilter;
